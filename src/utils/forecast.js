c64d05b68d1bd42d0ab6b3976930527436507fe2
David <david.whiteside85@gmail.com>
2020-04-17 14:35:38 +0100

    added weather description and modified to use openweather api

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude,longitude,callback) => {
-    const weatherurl  = 'http://api.weatherstack.com/current?access_key=b26750644c04dcc75363b7bcbab6c7c6&query=' + latitude + ',' +  longitude
+    const weatherurl  = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=b9c5ebdbc446ecbf28f1a21d3d700dcf'
     request({ url: weatherurl, json: true},(error,{body}) => {
         if (error) {
             callback('Unable to connect to weather service',undefined)
         } else if (body.error){ 
             callback('Unable to find location for weather, try another search',undefined)
         } else {
-            callback(undefined,body.current.weather_description[0] + '. The temperature is ' + body.current.temperature + 'c. It feels like ' + body.current.feelslike + 'c.')
+            callback(undefined,'The weather is ' + body.weather[0].description + '. The temperature is ' + body.main.temp + 'c. It feels like ' + body.main.feels_like + 'c.')
         }
-    })}
+    }
+)}
 
 module.exports = forecast
